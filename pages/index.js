31677e71d079c020f1b35e358d68facbc94a872f
gamingninjasjj-ops <gamingninjasjj@gmail.com>
2025-11-26 11:17:14 +1100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
+import NavBar from '../components/NavBar';
+
 export default function Home() {
   return (
     <div>
+      <NavBar />
       <h1>Welcome to Pocket PT</h1>
       <p>Your personal trainer in your pocket.</p>
     </div>
