8567a19c442e137a6689a991435e232a621110cb
gamingninjasjj-ops <gamingninjasjj@gmail.com>
2025-11-26 11:19:01 +1100

    Update survey.js

--- pages/survey.js
+++ pages/survey.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { supabase } from '../lib/supabaseClient';
+import NavBar from '../components/NavBar';
 
 export default function Survey() {
   const [formData, setFormData] = useState({
@@ -27,40 +28,22 @@ export default function Survey() {
     }
   };
 
-  if (submitted) {
-    return <div>Thank you! Your program will be generated soon.</div>;
-  }
-
   return (
     <div>
-      <h1>Survey</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name: <input name="name" value={formData.name} onChange={handleChange} required />
-        </label>
-        <br />
-        <label>
-          Age: <input name="age" value={formData.age} onChange={handleChange} required />
-        </label>
-        <br />
-        <label>
-          Experience Level: <input name="experience" value={formData.experience} onChange={handleChange} />
-        </label>
-        <br />
-        <label>
-          Equipment Available: <input name="equipment" value={formData.equipment} onChange={handleChange} />
-        </label>
-        <br />
-        <label>
-          Injuries or Issues: <input name="issues" value={formData.issues} onChange={handleChange} />
-        </label>
-        <br />
-        <label>
-          Goals: <input name="goals" value={formData.goals} onChange={handleChange} />
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
+      <NavBar />
+      {submitted ? (
+        <p>Thank you for completing the survey!</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <label>Name: <input type="text" name="name" value={formData.name} onChange={handleChange} /></label>
+          <label>Age: <input type="text" name="age" value={formData.age} onChange={handleChange} /></label>
+          <label>Experience: <input type="text" name="experience" value={formData.experience} onChange={handleChange} /></label>
+          <label>Equipment: <input type="text" name="equipment" value={formData.equipment} onChange={handleChange} /></label>
+          <label>Issues: <input type="text" name="issues" value={formData.issues} onChange={handleChange} /></label>
+          <label>Goals: <input type="text" name="goals" value={formData.goals} onChange={handleChange} /></label>
+          <button type="submit">Submit</button>
+        </form>
+      )}
     </div>
   );
 }
