aea9d72c5da4bc9739fd076347dd3060b71b70ee
gamingninjasjj-ops <gamingninjasjj@gmail.com>
2025-11-26 11:26:25 +1100

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,9 @@
+import NavBar from '../components/NavBar';
+
 export default function Dashboard() {
   return (
     <div>
+      <NavBar />
       <h1>Dashboard</h1>
       <p>Your training program will appear here once generated and you have subscribed.</p>
     </div>
